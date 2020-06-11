9e0fec10f2e18cc5ef091940a046cbd47dc06601
gdupont <gdupont@student.42.fr>
2020-06-11 10:49:39 +0200

    wip

--- srcs/lst_manage.c
+++ srcs/lst_manage.c
@@ -6,7 +6,7 @@
 /*   By: gdupont <gdupont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/08 15:03:33 by gdupont           #+#    #+#             */
-/*   Updated: 2020/06/11 09:35:18 by gdupont          ###   ########.fr       */
+/*   Updated: 2020/06/11 10:49:17 by gdupont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ int		envp_to_list(t_env **env, char **envp)
 	int		i;
 	t_env	*new;
 
-	if (!(*env = set_up_elem(" = ", NOT_EXPORT)))
+	if (!(*env = set_up_elem("?= ", NOT_EXPORT)))
 		return (-1);
 	i = 0;
 	while (envp[i])
